cfe6ad9d16e2555738edc4837268252a696251fa
w2wcr <webservices@w2wcr.org>
2024-09-14 12:36:44 -0400

    Update config.js to W2WCR callsign

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const topBarCenterText = `VA3HDL - FN04ga`;
+const topBarCenterText = `W2WCR - FN33eh`;
 // Menu items
 // Structure is as follows HTML Color code, Option, target URL, scaling 1=Original Size, side (optional, nothing is Left, "R" is Right)
 // The values are [color code, menu text, target link, scale factor, side],
